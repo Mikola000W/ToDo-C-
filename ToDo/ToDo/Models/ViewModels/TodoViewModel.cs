0eaa4029f620eedb15b28dfda617db74a7064654
Mikola000W <113891277+Mikola000W@users.noreply.github.com>
2023-04-12 15:57:52 +0200

    Add files via upload

---
+++ ToDo/ToDo/Models/ViewModels/TodoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ToDo.Models.ViewModels
+{
+    public class TodoViewModel 
+    {
+        public List<ToDoItem> TodoList { get; set; }
+        public ToDoItem Todo { get; set; }
+    }
+}
---
+++ ToDo/ToDo/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿function deleteTodo(i) {
+    $.ajax({
+        url: 'Home/Delete',
+        type: 'POST',
+        data: {
+            id: i
+        },
+        success: function () {
+            window.Location.reload();
+        }
+    });
+}
+function populateForm(i) {
+    $.ajax({
+        url: 'Home/PopulateForm',
+        type: 'GET',
+        data: {
+            id: i
+        },
+        dataTypeL ,'json',
+        success: function (response) {
+            $("#Todo_Name").val(response.name);
+            $("#Todo_Id").val(response.id);
+            $("#Todo-button").val("Update Todo");
+            $("#form-action").attr("action", "Home/Update");
+        }
+
+    })
+}
